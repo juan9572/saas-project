f5cd31b0a12d0b23592e02a1b22ce03605f72c08
Juan Pablo Rincon Usma <68928340+juan9572@users.noreply.github.com>
2025-12-21 18:30:00 -0500

    Bootstrap Spring Boot project (#1)
    * doc(diagrams): add diagrams of solution
    
    * chore(subscription-platform): add initial config of project

---
+++ subscription-platform/src/test/java/com/sas/platform/SubscriptionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sas.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
